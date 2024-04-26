1098226eb1ab5279f2368bc3874ad648562b379f
Gustavo A. Fossati <gustavo.fossati@alu.inspt.utn.edu.ar>
2024-04-26 19:48:56 -0300

    tercer repo

--- sumado.c
+++ sumado.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-
+/*Nueva version con tressumandos*/
 int main()
 {
-    int num1, num2, suma;
+    int num1, num2, num3, suma;
     printf("Ingrese un primer valor: ");
     scanf("%d", &num1);
     printf("Ingrese un segundo valor: ");
     scanf("%d", &num2);
-    suma = num1 + num2;
+    printf("Ingrese un tercer valor: ");
+    scanf("%d", &num3);
+    suma = num1 + num2 + num3;
 
-    printf("%d + %d = %d ", num1, num2, suma);
+    printf("%d + %d + %d = %d ", num1, num2, num3, suma);
 
     return 0;
 }
